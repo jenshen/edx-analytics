6b4f9b3adffe450e3cb484cc7bbd4bd94be414f9
codyaustun <colemanc@mit.edu>
2014-05-02 10:37:13 -0400

    Added Country and EducationLevels collections

--- server/startup.js
+++ server/startup.js
@@ -1,27 +1,81 @@
 var root = typeof exports !== "undefined" && exports !== null ? exports : this;
 
 Meteor.startup(function() {
-  // CourseAxis.remove({});
   if (CourseAxis.find().count() === 0){
     root.CourseAxisFactory.create(courses[0]);
   }
 
-  course = CourseAxis.findOne({category: 'course'})
-  video = CourseAxis.findOne({
-    category: 'video',
-    course_id: course.course_id
-  })
+  // course = CourseAxis.findOne({category: 'course'})
+  // video = CourseAxis.findOne({
+  //   category: 'video',
+  //   course_id: course.course_id
+  // })
 
-  root.VideoActivityGenerator.create({
-    video: video,
-    course: course,
-    educationLevels: educationLevels,
-    countries: countries,
-    scalingConstant: 10000,
-    SNR: 10
-  })
+  // root.VideoActivityGenerator.create({
+  //   video: video,
+  //   course: course,
+  //   educationLevels: educationLevels,
+  //   countries: countries,
+  //   scalingConstant: 10000,
+  //   SNR: 10
+  // })
   
-  // Visualizations.remove({});
+  if (EducationLevels.find().count() === 0){
+    EducationLevels.insert({
+      'name': 'PhD',
+      'short': 'p',
+      'scale': 0.15
+    });
+    EducationLevels.insert({
+      'name': 'Masters',
+      'short': 'm',
+      'scale': 0.15
+    });
+    EducationLevels.insert({
+      'name': 'High School',
+      'short': 'hs',
+      'scale': 0.25
+    });
+    EducationLevels.insert({
+      'name': 'Bachelors',
+      'short': 'b',
+      'scale': 0.25
+    });
+    EducationLevels.insert({
+      'name': 'Other',
+      'short': 'o',
+      'scale': 0.2
+    });
+  }
+
+  if (Country.find().count() === 0){
+    Country.insert({
+      'name': 'United States',
+      'short': 'US',
+      'scale': 1.0
+    });
+    Country.insert({
+      'name': 'United Kingdom',
+      'short': 'UK',
+      'scale': 0.3
+    });
+    Country.insert({
+      'name': 'Spain',
+      'short': 'ES',
+      'scale': 0.45
+    });
+    Country.insert({
+      'name': 'China',
+      'short': 'CN',
+      'scale': 0.15
+    });
+    Country.insert({
+      'name': 'Brazil',
+      'short': 'BR',
+      'scale': 0.35
+    });
+  }
+
   if (Visualizations.find().count() === 0) {
     Visualizations.insert({
       name: 'Course Summary',
--- shared/collections/app.js
+++ shared/collections/app.js
@@ -1,3 +1,5 @@
 Visualizations = new Meteor.Collection("visualization");
 CourseAxis = new Meteor.Collection("course_axis");
 ViewCount = new Meteor.Collection("view_count");
+Country = new Meteor.Collection("country");
+EducationLevels = new Meteor.Collection("loe");
