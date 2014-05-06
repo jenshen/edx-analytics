ecc6d41fa13595cae6cd2076bd0df721e1a80ffe
codyaustun <colemanc@mit.edu>
2014-05-06 09:17:44 -0400

    Saving work

--- client/views/visualization.js
+++ client/views/visualization.js
@@ -12,6 +12,33 @@ Template.visualization.events = {
   },
   'click #download': function(event){
     console.log('Download');
+    params = {
+      pipe: [{
+        $match: {
+          module_id: {$in: ["MITx/Spring_2014/test_course1/C0_LS1_V3"]},
+          loe: {$in: ["p"]},
+          date: {
+            $gt: "2020-02-03 05:00:00",
+            $lt: "2020-02-05 05:00:00"
+          },
+          cc: {$in: ["US"]}
+        }
+      },{
+        $group: {
+          _id: {date: "$date"},
+          count: {$sum: "$count"}
+        }
+      }],
+      collection: 'daily_count'
+    }
+    console.log(this)
+    Meteor.call('aggregate',
+      params,
+      function (error, result) {
+        csvContent = "data:text/csv;charset=utf-8,"
+        console.log(error);
+        console.log(result);
+    }) 
   }
 };
 Template.visualization.figures = function(){
