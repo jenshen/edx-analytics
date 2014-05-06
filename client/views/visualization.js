5b1b806552d5ce8dca3b185337d3e525e057f666
cgn <cgn@mit.edu>
2014-05-06 03:22:49 -0400

    Add unfavorite and favorite feedback. Added searchbar and search. Added search feedback. Added responsive UI. Added media queries that adjust header. Added the Share functionality. Added styling to small screen size nav bar. Added Welcome to PhilosphyX.

--- client/views/visualization.js
+++ client/views/visualization.js
@@ -4,10 +4,16 @@ Template.visualization.rendered = function () {
       UI.render(Template.test), this.find('.data')
   );
 };
+
 Template.visualization.events = {
   'click #favorite': function(event){
     console.log('Favorited');
-    Visualizations.update(this._id, {$set: {favorite: true}});
+    var isFavorited = Visualizations.findOne({_id:this._id}).favorite;
+    if (isFavorited) {
+      Visualizations.update(this._id, {$set: {favorite: false}});
+    } else {
+      Visualizations.update(this._id, {$set: {favorite: true}});
+    }
   },
   'click #share': function(event){
     console.log('Share');
