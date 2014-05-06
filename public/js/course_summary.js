95cb7eec52c93dc0c224b56951e7756a3559fec7
ericadu <ericadu@mit.edu>
2014-05-06 12:58:24 -0400

    fiddling with graphs

--- public/js/course_summary.js
+++ public/js/course_summary.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   // Wrapping in nv.addGraph allows for '0 timeout render', stores rendered charts in nv.graphs, and may do more in the future... it's NOT required
-  var set_1 = [{x: 0, y: 5}, {x: 1, y: 1}, {x: 2, y: 2}];
+  var set_1 = [{x: 1025409600000, y: 5}, {x: 1122782400000, y: 1}, {x: 1304136000000, y: 2}];
   
   var data = [{"key":"DOWN", "values": set_1, "color": "#2F73BC"}];
 
@@ -15,14 +15,17 @@ $(document).ready(function () {
       showLegend: false,
     })
     .tooltipContent(function (key, x, y, e, graph) {
-         return "<h3 class='tool-tip'>" + x + '</h3>' +"<p class='tool-tip'>" + y + '</p>' ;
+         return "<h3 class='tool-tip'>" + x + '</h3>' +"<p class='tool-tip'>" + y + " views" +'</p>' ;
         });
     ;
 
     // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
     chart.xAxis
       .axisLabel("Total Views")
-      .tickFormat(d3.format(',.1f'))
+      //.tickFormat(d3.format(',.1f'))
+      .tickFormat(function(d) {
+            return d3.time.format('%x')(new Date(d))
+          });
       ;
 
     chart.yAxis
@@ -136,7 +139,49 @@ $(document).ready(function () {
   });
 
 
+  var set_5 = [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 1}, {x: 3, y: 2}, {x: 4, y: 4}, {x: 5, y: 10}];
+  
+  var set5_data = [{"key": "Total Attempts", "values": set_5, "color": "#d21673"}];
 
+  nv.addGraph(function() {
+    var chart = nv.models.lineChart()
+    .options({
+      margin: {left: 30, bottom: 30},
+      x: function(d,i) { return i},
+      showXAxis: false,
+      showYAxis: true,
+      transitionDuration: 250,
+      showLegend: false
+    })
+    .tooltipContent(function (key, x, y, e, graph) {
+         return "<h3 class='tool-tip'>" + x + '</h3>' +"<p class='tool-tip'>" + y + '</p>' ;
+        });
+    ;
+
+    // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
+    chart.xAxis
+      .axisLabel("Total Minutes")
+      //.tickFormat(function (d) {return '';})
+      .tickFormat(d3.format(',.1f'))
+      ;
+
+    chart.yAxis
+      .axisLabel("Minutes")
+      .tickFormat(d3.format(',.f'))
+      ;
+
+    d3.select('#chart4 svg')
+      .datum(set5_data)
+      .call(chart);
+
+    //TODO: Figure out a good way to do this automatically
+    nv.utils.windowResize(chart.update);
+    //nv.utils.windowResize(function() { d3.select('#chart1 svg').call(chart) });
+
+    chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+
+    return chart;
+  });
 
   var pie_data = [{"label": "Male", "value": 1000234}, {"label": "Female", "value": 999200}, {"label": "Unreported", "value": 20000}];
 
