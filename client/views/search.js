1c279cf0d5addc8f286cc73dddd3fd0f2584ecc7
cgn <cgn@mit.edu>
2014-05-06 17:45:10 -0400

    small modification to search

--- client/views/search.js
+++ client/views/search.js
@@ -2,12 +2,12 @@ Template.search.events = {
   'keypress input#search-box': function (evt, template) {
         if (evt.which === 13) {
 	      	var query = template.find("#search-box").value;
+	      	$("#search-box").value = "";
 	      	query = ".*"+ query +".*$";
-	      	console.log(query);
 	      	var result = Visualizations.findOne({"name" : {$regex : query, $options: 'i'}});
 	    	console.log(result);
 	    	$("div.data").html('<h2>Success! You selected <b>' + result.name+ '</b></h2>');
-	    	$("#search-box").val('');
+	    	
 
 
 	    	//Cody - add code here
@@ -15,8 +15,5 @@ Template.search.events = {
 	    	//visualization to be added.
 	    	//if it is undefined, do nothing!
         }	
-    },
-    'click input#search-box': function() {
-    	console.log('click');
     }
 };
