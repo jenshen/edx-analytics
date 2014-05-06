6d8d1fce0353dc04d78082f670d9137ee4907e66
cgn <cgn@mit.edu>
2014-05-06 18:28:22 -0400

    Results from searches now display graphs in main viewing area. Font looks is more readable in off canvas.

--- client/views/search.js
+++ client/views/search.js
@@ -5,15 +5,8 @@ Template.search.events = {
 	      	$("#search-box").value = "";
 	      	query = ".*"+ query +".*$";
 	      	var result = Visualizations.findOne({"name" : {$regex : query, $options: 'i'}});
-	    	console.log(result);
-	    	$("div.data").html('<h2>Success! You selected <b>' + result.name+ '</b></h2>');
-	    	
-
-
-	    	//Cody - add code here
-	    	//result containst he database object of the 
-	    	//visualization to be added.
-	    	//if it is undefined, do nothing!
-        }	
+	    	//$("div.data").html('<h2>Success! You selected <b>' + result.name+ '</b></h2>');
+	    	Session.set("selected", result._id);
+	    }
     }
 };
