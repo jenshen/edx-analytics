71e3976970a1c8968bec502f92fce9d154ae39a7
cgn <cgn@mit.edu>
2014-05-06 20:47:28 -0400

    Improved search - displays no results, fades away, resets input value. Added spacing to graphs and buttons.

--- client/views/search.js
+++ client/views/search.js
@@ -1,12 +1,18 @@
 Template.search.events = {
-  'keypress input#search-box': function (evt, template) {
+  'keypress #search-box': function (evt, template) {
         if (evt.which === 13) {
+	      	var selector = $('#visualizations>ul>input');
 	      	var query = template.find("#search-box").value;
-	      	$("#search-box").value = "";
 	      	query = ".*"+ query +".*$";
 	      	var result = Visualizations.findOne({"name" : {$regex : query, $options: 'i'}});
 	    	//$("div.data").html('<h2>Success! You selected <b>' + result.name+ '</b></h2>');
-	    	Session.set("selected", result._id);
+	    	if (typeof result != 'undefined'){
+		        Session.set("selected", result._id);
+		        selector.val('');
+		    } else {
+		    	selector.val('Sorry, no matches.');
+		    	setTimeout(function() {selector.val('');}, 800);		    	
+		    }
 	    }
     }
 };
